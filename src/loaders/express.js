d55322c17c51c80c3d6d0aecc8aab82a5bad0fc3
Yuriy <yrambler2001@gmail.com>
2021-12-07 20:51:32 +0200

    feat(co2 backend): added proxy to co2 backend

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -15,6 +15,15 @@ const proxyToSpotifyServer = proxy('http://localhost:5000/graphql', {
   },
 });
 
+const proxyToCO2Server = proxy('http://localhost:5100/graphql', {
+  filter(req) {
+    return ['co2Data'].includes(req?.body?.operationName);
+  },
+  proxyReqPathResolver(req) {
+    return req.baseUrl;
+  },
+});
+
 export default ({ app }) => {
   app.get('/status', (req, res) => {
     res.status(200).end();
@@ -29,6 +38,7 @@ export default ({ app }) => {
   app.use(express.static(path.join(process.cwd(), 'public')));
 
   app.use('/graphql', proxyToSpotifyServer);
+  app.use('/graphql', proxyToCO2Server);
 
   apolloLoader({ app });
   app.use((req, res, next) => {
